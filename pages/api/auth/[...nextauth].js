fa8320a4937bf8020f536c9e48378e31f4d0f324
kailash8799 <kailashrajput8799@gmail.com>
2023-05-20 19:42:17 +0530

    added secret key

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,6 +1,6 @@
-import NextAuth from "next-auth"
-import GoogleProvider from "next-auth/providers/google"
-import { getToken } from "next-auth/jwt"
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import { getToken } from "next-auth/jwt";
 
 export default NextAuth({
   // Configure one or more authentication providers
@@ -10,22 +10,29 @@ export default NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
     // ...add more providers here
-    ],
-    
-    pages:{
-        signIn:"/auth/signin"
+  ],
+
+  pages: {
+    signIn: "/auth/signin",
   },
-  callbacks:{
-    async session({session, token, user}){
-      session.user.username = session?.user?.name.split(" ").join('').toLocaleLowerCase();
-      session.user.uid = token?.sub
+  debug:process.env.NODE_ENV==='development',
+  session:{
+    strategy:'jwt'
+  },
+  secret:process.env.NEXTAUTH_SECRET,
+  callbacks: {
+    async session({ session, token, user }) {
+      session.user.username = session?.user?.name
+        .split(" ")
+        .join("")
+        .toLocaleLowerCase();
+      session.user.uid = token?.sub;
       return session;
-    }
+    },
   },
-  
-})
-        //   theme:{
-        //     logo:"https://papareact.com/sq0",
-        //     brandColor:"#F13287",
-        //     colorScheme:"auto"
-        //   }
+});
+//   theme:{
+//     logo:"https://papareact.com/sq0",
+//     brandColor:"#F13287",
+//     colorScheme:"auto"
+//   }
